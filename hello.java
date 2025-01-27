ecba3a5e5b950a21d788f7b6a8cbb81957433117
vandit <jaiswalvandit25@gmail.com>
2025-01-27 22:33:54 +0530

    new feat added

---
+++ hello.java
@@ -0,0 +1,14 @@
+package Lab030utk;
+
+class hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+    }
+
+    int x =2;
+    int y=3;
+
+    int sum = x+y;
+    System.out.println(sum);
+}
